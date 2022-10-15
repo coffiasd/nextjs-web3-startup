fd654e642bd836d2ebb119ba0f7d7ea87b244ab2
coffiasd <759291502@qq.com>
2022-10-15 22:43:34 +0800

    fix:footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,21 @@
+import styles from '../styles/Home.module.css';
 
 export default function Footer() {
     return (
         <footer className="footer p-10 bg-base-300 text-base-content">
             <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <span className="footer-title text-black">Services</span>
+                <a className={styles.footerHover}>BRAND</a>
+                <a className={styles.footerHover}>Design</a>
+                <a className={styles.footerHover}>Marketing</a>
+                <a className={styles.footerHover}>Advertisement</a>
             </div>
             <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <span className="footer-title text-black">Company</span>
+                <a className={styles.footerHover}>About us</a>
+                <a className={styles.footerHover}>Contact</a>
+                <a className={styles.footerHover}>Jobs</a>
+                <a className={styles.footerHover}>Press kit</a>
             </div>
             <div>
                 <span className="footer-title">Social</span>
